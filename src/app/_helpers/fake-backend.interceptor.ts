9c7364700bd90f9a120abaf2f473a6eda4f8ed83
Siddartha Mote <siddart14393@mastek.com>
2022-01-15 15:53:15 +0530

    Init chang for fake, alert and user mod w acc serv

--- src/app/_helpers/fake-backend.interceptor.ts
+++ src/app/_helpers/fake-backend.interceptor.ts
@@ -3,16 +3,66 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, materialize, dematerialize } from 'rxjs/operators';
+import { User } from '@app/_models/user';
+
+// array in local storage for registered users
+const usersKey: string = 'angular-10-registration-login-example-users';
+let userInfo = localStorage.getItem(usersKey);
+let users: User[] = userInfo ? JSON.parse(userInfo) : [];
 
 @Injectable()
 export class FakeBackendInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const { url, method, headers, body } = request;
+
+    return handleRoute();
+
+    function handleRoute() {
+      switch (true) {
+        case url.endsWith('/users/authenticate') && method === 'POST':
+          return authenticate();
+        default:
+          // pass through any requests not handled above
+          return next.handle(request);
+      }
+
+    }
+
+    function authenticate() {
+      const username = body;
+      const password = body;
+      const user = users.find(x => x.username === username && x.password === password);
+      if (!user) return error('Username or password is incorrect');
+      return ok({
+        ...basicDetails(user),
+        token: 'fake-jwt-token'
+      })
+    }
+
+    // helper functions
+
+    function ok(body?: any) {
+      return of(new HttpResponse({ status: 200, body }))
+        .pipe(delay(500)); // delay observable to simulate server api call
+    }
+
+    function error(message: any) {
+      return throwError({ error: { message } })
+        .pipe(materialize(), delay(500), dematerialize()); // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648);
+    }
+
+    function basicDetails(user: User) {
+      const { id, username, firstName, lastName } = user;
+      return { id, username, firstName, lastName };
+    }
+
   }
 }
